fcd7f7e4e84e6dcb711a8fbe1cec661d16fcf904
xSmaiL420 <tnknight97@gmail.com>
2024-04-01 11:25:27 +0200

    changes the getComponentData function to actually get the saved data from firestore/added saving any retreived components data directly in firestore

--- src/utils/components.utils.ts
+++ src/utils/components.utils.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 import { ComponentModel } from "../models/component.module"; // Import the ComponentModel interface
 import config from "config";
+import { Firestore } from "@google-cloud/firestore";
 
 interface BudgetPercentages {
   [key: string]: {
@@ -11,7 +12,8 @@ interface BudgetPercentages {
 
 // Define a function to fetch component data for a specific component type from the API
 async function fetchComponentData(
-  componentType: string
+  componentType: string,
+  db: Firestore
 ): Promise<ComponentModel[]> {
   try {
     const response = await axios.post(
@@ -20,6 +22,12 @@ async function fetchComponentData(
         filscateg: { titre: componentType },
       }
     );
+
+    response.data.map(async (item: any) => {
+      //save in firestore
+      const buildRef = db.collection("Components").doc(item.lien);
+      await buildRef.set(item);
+    });
 
     // Extract relevant information from the response and map it to ComponentModel interface
     const components: ComponentModel[] = response.data.map((item: any) => ({
@@ -37,14 +45,18 @@ async function fetchComponentData(
   }
 }
 
-export async function getComponentData(lien : string): Promise<any> {
+export async function getComponentData(
+  lien: string,
+  db: Firestore
+): Promise<any> {
   try {
-    const response = await axios.get(
-      `${config.get("MEGAPC_CLIENT_API")}/produit/byTitleOne/${lien}`
-    );
+    const componentRef = db.collection("Components").doc(lien);
+    const componentDoc = await componentRef.get();
+    console.log("componentDoc", componentDoc.data());
+    
 
     // Extract relevant information from the response and map it to ComponentModel interface
-    const component = response.data
+    const component = componentDoc.data();
 
     return component;
   } catch (error) {
@@ -67,27 +79,28 @@ export const componentTypes = [
 ];
 // Define a function to filter components by price range
 export async function filterComponentsByBudget(
-  userBudget: number
+  userBudget: number,
+  db: Firestore
 ): Promise<{ [key: string]: ComponentModel[] }> {
   const filteredComponents: { [key: string]: ComponentModel[] } = {};
 
   // Define budget allocation percentages for each component type
   const budgetPercentages: BudgetPercentages = {
     PROCESSEUR: { min: 0.15, max: 0.3 }, // Processor/CPU
-    REFROIDISSEMENT: { min: 0.05, max: 0.12 }, // Cooling solution (e.g., CPU cooler)
+    REFROIDISSEMENT: { min: 0.05, max: 0.12 }, // Cooling solution
     "CARTE MÈRE": { min: 0.08, max: 0.15 }, // Motherboard
     "BARETTE MÉMOIRE": { min: 0.05, max: 0.15 }, // Memory/RAM
     ALIMENTATION: { min: 0.03, max: 0.15 }, // Power Supply Unit (PSU)
     "DISQUE-SSD": { min: 0.01, max: 0.25 }, // Solid State Drive (SSD)
     "DISQUE-HDD": { min: 0.03, max: 0.1 }, // Hard Disk Drive (HDD)
-    "DISQUE-NVME": { min: 0.03, max: 0.1 }, // NVMe SSD (faster storage)
+    "DISQUE-NVME": { min: 0.03, max: 0.1 }, // NVMe SSD
     VENTILATEUR: { min: 0.03, max: 0.07 }, // Additional case fans or CPU cooling
     "CARTE GRAPHIQUE": { min: 0.15, max: 0.3 }, // Graphics Card (GPU)
   };
 
   // Iterate over each component type and fetch component data
   for (const componentType of componentTypes) {
-    const components = await fetchComponentData(componentType);
+    const components = await fetchComponentData(componentType, db);
 
     // Calculate the price range for the current component type based on the user's budget
     const priceRange = {
