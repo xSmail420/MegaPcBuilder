d55a26ab0b6d0887cc71ed2b7ac066e8a489328c
xSmaiL420 <tnknight97@gmail.com>
2024-04-05 18:58:59 +0100

    adjusted the model response format to include the case as one of the components

--- src/utils/aibuilder.utils.ts
+++ src/utils/aibuilder.utils.ts
@@ -15,13 +15,12 @@ export async function generateComputerBuild(
         The AI assistant is highly proficient in understanding user requirements and constraints, adept at balancing performance, budget, and preferences to generate the most suitable computer builds.
         Your response should be in this format:
 \n\n
-{ 'PROCESSEUR' : { 'lien': string }, 'REFROIDISSEMENT': { 'lien': string }, 'CARTE MÈRE': { 'lien': string }, 'BARETTE MÉMOIRE': { 'lien': string }, 'ALIMENTATION': { 'lien': string }, 'STORAGE': { 'lien': string },'VENTILATEUR': { 'lien': string }, 'CARTE GRAPHIQUE': { 'lien': string } }
+{ 'PROCESSEUR' : { 'lien': string }, 'CARTE MÈRE': { 'lien': string }, 'BARETTE MÉMOIRE': { 'lien': string }, 'ALIMENTATION': { 'lien': string }, 'STORAGE': { 'lien': string },'BOITIER': { 'lien': string }, 'CARTE GRAPHIQUE': { 'lien': string } }
 \n\n
         START CONTEXT BLOCK
         ${JSON.stringify(context)}
         END OF CONTEXT BLOCK
         EVERY "lien: " attribute in the response is provided in the CONTEXT BLOCK under it's own category.
-        'REFROIDISSEMENT' and 'VENTILATEUR' should never be the same components.
         AI assistant will take into account any CONTEXT BLOCK that is provided in a conversation.
         AI assistant will always follows the response format provided.
         AI assistant will not mismatch a components type with another from the context.
